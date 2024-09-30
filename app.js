9821238bc9c15bbe920a7f520cc91d2a3276a3a3
shakibkumnale <shakibkumnali@gmail.com>
2024-09-30 18:42:45 +0530

    fix cors issue2

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 10211;
 
 app.use(express.json()); // to parse JSON request body
-app.options('*', cors()); // Enable pre-flight requests for all routes
+app.use(cors({
+  origin: '*', // Allows requests from any origin
+  methods: ['GET', 'POST'], // Specify allowed methods
+  credentials: false // Disable credentials (e.g., cookies, authorization headers) for security purposes
+})) // Enable pre-flight requests for all routes
 
 app.use(express.urlencoded({extended:true}))
 
